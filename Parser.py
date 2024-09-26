b7dbc6caad66012009ae44c2c1503f18809a2d9f
Yeah Jack <95103974+Yeah-Jack@users.noreply.github.com>
2024-09-26 16:23:42 +0200

    Format for better readability

--- Parser.py
+++ Parser.py
@@ -1,6 +1,6 @@
 # Open the input text file
 with open('input.txt', 'r') as file:
-    text = file.read()
+	text = file.read()
 
 # Split the text into sentences
 import re
@@ -9,14 +9,14 @@ sentences = re.split(r'(?<=[^.?!])\s+', text)
 # Format the sentences
 formatted_text = []
 for sentence in sentences:
-    if sentence.endswith('.'):
-        formatted_text.append(sentence)
-    else:
-        if formatted_text and not formatted_text[-1].endswith('.'):
-            formatted_text[-1] += ' ' + sentence
-        else:
-            formatted_text.append(sentence)
+	if sentence.endswith('.'):
+		formatted_text.append(sentence)
+	else:
+		if formatted_text and not formatted_text[-1].endswith('.'):
+			formatted_text[-1] += ' ' + sentence
+		else:
+			formatted_text.append(sentence)
 
 # Write the formatted text to a new file
 with open('output.txt', 'w') as file:
-    file.write('\n'.join(formatted_text))
+	file.write('\n'.join(formatted_text))
